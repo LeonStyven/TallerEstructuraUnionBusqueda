415ee67aafb07251b14b945af904aca9cc356f88
JulianG91012 <juliangomez91012@gmail.com>
2022-03-28 17:57:34 -0500

    Bug fixed

--- src/tallerestructuraunionbusqueda/TallerEstructuraUnionBusqueda.java
+++ src/tallerestructuraunionbusqueda/TallerEstructuraUnionBusqueda.java
@@ -24,72 +24,65 @@ public class TallerEstructuraUnionBusqueda {
         //Switch con opciones para determinar tamaño de n de cada vector a crear.
         String respuesta = "";
         Punto2D[] puntos = null; // Arreglo de puntos leidos del archivo
-        
-        StdOut.println("Para crear el vector, ingrese el número del tamaño que desea según las opciones dadas para el vector de Puntos2D:\n");
-        StdOut.println("Los tamaños soportados son de: \n");
-        StdOut.println("1. 100\n");
-        StdOut.println("2. 120\n");
-        StdOut.println("3. 150\n");
-        StdOut.println("4. 200\n");
-        StdOut.println("5. 1000\n");
-        StdOut.println("6. 2500\n");
-        StdOut.println("7. 5000\n");
+        File doc = new File("");
+        final double DMAX = 0.3; //Se elige la distancia máxima entre los puntos
         
         
-        switch(respuesta)
-                {
-                    case "1": 
-                        File doc = new File("ejemplos\\datapoints-100.csv");
-                        leerArchivo(doc);
-                         puntos = new Punto2D[100];
-                        break;
-                    case "2":
-                        File doc1 = new File("ejemplos\\datapoints-120.csv");
-                        leerArchivo(doc1);
-                        puntos = new Punto2D[120];
-                        break;
-                    case "3":
-                        File doc2 = new File("ejemplos\\datapoints-150.csv");
-                        leerArchivo(doc2);
-                        puntos = new Punto2D[150];
-                        break;
-                    case "4":
-                        File doc3 = new File("ejemplos\\datapoints-200.csv");
-                        leerArchivo(doc3);
-                        puntos = new Punto2D[200];
-                        break;
-                    case "5":
-                        File doc4 = new File("ejemplos\\datapoints-1000.csv");
-                        leerArchivo(doc4);
-                        puntos = new Punto2D[1000];
-                        break;
-                    case "6":
-                        File doc5 = new File("ejemplos\\datapoints-2500.csv");
-                        leerArchivo(doc5);
-                        puntos = new Punto2D[2500];
-                        break;
-                    case "7":
-                        File doc6 = new File("ejemplos\\datapoints-5000.csv");
-                        leerArchivo(doc6);
-                        puntos = new Punto2D[5000];
-                        break;
-                    default:
-                        StdOut.println("Ha ingresado una opción inválida.");
-                        break;
-                }
-        
-        }
-        public static void leerArchivo(File doc){
             //Obtener el archivo separado por comas
             BufferedReader bufferLectura = null;
-            Punto2D[] puntos = null; // Arreglo de puntos leidos del archivo
             final String SEPARADOR = ",";
 
             //FALTA HACER QUE SE SELECCIONE
             //File doc = new File("//ejemplos\\datapoints-100.csv"); 
 
             try{
+                StdOut.println("Para crear el vector, ingrese el número del tamaño que desea según las opciones dadas para el vector de Puntos2D:\n");
+                StdOut.println("Los tamaños soportados son de: \n");
+                StdOut.println("1. 100\n");
+                StdOut.println("2. 120\n");
+                StdOut.println("3. 150\n");
+                StdOut.println("4. 200\n");
+                StdOut.println("5. 1000\n");
+                StdOut.println("6. 2500\n");
+                StdOut.println("7. 5000\n");
                 int counter = 0;//Contador de puntos
+                //Ciclo cuando el usuario ingrese una opción inválida
+                
+                switch(respuesta)
+                {
+                     case "1": 
+                        doc = new File("ejemplos\\datapoints-100.csv");
+                         puntos = new Punto2D[100];
+                        break;
+                    case "2":
+                        doc = new File("ejemplos\\datapoints-120.csv");
+                        puntos = new Punto2D[120];
+                        break;
+                    case "3":
+                        doc = new File("ejemplos\\datapoints-150.csv");
+                        puntos = new Punto2D[150];
+                        break;
+                    case "4":
+                        doc = new File("ejemplos\\datapoints-200.csv");
+                        puntos = new Punto2D[200];
+                        break;
+                    case "5":
+                        doc = new File("ejemplos\\datapoints-1000.csv");
+                        puntos = new Punto2D[1000];
+                        break;
+                    case "6":
+                        doc = new File("ejemplos\\datapoints-2500.csv");
+                        puntos = new Punto2D[2500];
+                        break;
+                    case "7":
+                        doc = new File("ejemplos\\datapoints-5000.csv");
+                        puntos = new Punto2D[5000];
+                        break;
+                    default:
+                        StdOut.println("La opción ingresada es incorrecta");
+                        break;
+                }
+                
 
                 // Abrir el .csv en buffer de lectura
                 bufferLectura = new BufferedReader(new FileReader(doc)); //ejemplos\\datapoints-100.csv
