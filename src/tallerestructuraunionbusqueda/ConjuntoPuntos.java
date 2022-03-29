e94123fc193b87f61d0a3c80f9cdd00c61a82649
Leon Styven <j.stca03@gmail.com>
2022-03-28 21:24:18 -0500

    Comments, ConjuntoPuntos methods implemented and bug fixes

--- src/tallerestructuraunionbusqueda/ConjuntoPuntos.java
+++ src/tallerestructuraunionbusqueda/ConjuntoPuntos.java
@@ -15,10 +15,23 @@ public class ConjuntoPuntos {
     private int count;
     private Punto2D[] conjunto;
     private int[] parent;
+
+    //Accesores para los vectores
+    public Punto2D[] getConjunto() {
+        return conjunto;
+    }
+
+    public int[] getParent() {
+        return parent;
+    }
+
+    public int[] getSize() {
+        return size;
+    }
     private int[] size;
 
-    //Constructor pasandole un arrego de puntos
-    public ConjuntoPuntos(Punto2D[] conjunto)
+    //Al constructor se le debe pasar un arreglo donde se encuentran los puntos que se quieren convertir en un conjunto
+    public ConjuntoPuntos(Punto2D[] conjunto, double DMAX)
     {
         n = conjunto.length;
         count = n;
@@ -31,36 +44,43 @@ public class ConjuntoPuntos {
             parent[i] = i;
             size[i] = 1;
         }
+        
+    //CLUSTERIZAR LOS NODOS
+    Convertir(DMAX);
     }
     
+    //Obtener el arreglo de padres
+    
+    
+    //Analizar si 2 puntos ya estan conectados
     public boolean Connected(Punto2D p, Punto2D q){
         
-        //Un punto se conecta a otro cambiando su representante por el de su padre 
-        if(p.getRepresentante().equals(q.getRepresentante()))
+        //Un punto esta conectado con otro si sus padres son los mismos
+        if(Find(p)==Find(q))
             return true;    
         else
             return false;
     }
     
+    //Encontrar el indice donde se encuentra el padre
     public int Find(Punto2D p){
         int result = -1;
         for(int i = 0; i < conjunto.length; i++){
-        if (conjunto[i].equals(p)) result = i;
+            if (conjunto[i].equals(p)) result = i;
         }
-        
         return result;
     }
-    public int Count(Punto2D p)
-    {
-        //Cuenta cuántos puntos totales hay
-        return count;
-        
-    }
     
-    //Para unir dos puntos se cambia el representante de uno de ellos.
-    //Se debe comparar que esten a la distancia correcta
+    //Contar la cantidad de arboles
+    public int Count(Punto2D p)
+    {return count;}
+    
+    //unir 2 nodos del arbol
+    
     public void Union(Punto2D p, Punto2D q) 
     {
+    //Para unir dos puntos se cambia el representante de uno de ellos.
+    //Se debe comparar que esten a la distancia correcta
     //Buscar las raices y comparar que los nodos esten separados
     int rootP = Find(p);
     int rootQ = Find(q);
@@ -80,17 +100,15 @@ public class ConjuntoPuntos {
 
     }
     
-    public void Convertir(){
-    TallerEstructuraUnionBusqueda taller = new TallerEstructuraUnionBusqueda(); //Llamar a la clase donde esta el valor maximo de distancia
-        
+    //Recibe el parametro del maximo de distancia para unir
+    public void Convertir(double DMAX){
         for(int i = 0; i<n; i++){
             for(int j = i+1; i<n; i++){
-            //Validar que el tamaño este en el rango ni sea el mismo punto
-            /*
-            if((conjunto[i].distancia(conjunto[i+1]) <= taller.DMAX) && conjunto[i].distancia(conjunto[i+1])!=0){
-                Union(conjunto[i],conjunto[i+1]);
-                i++;
-            }*/
+                //Se valida que el tamaño este en el rango y que el nodo no esté unido a otro
+                if((conjunto[i].distancia(conjunto[j]) <= DMAX && //Comprobar el rango
+                        j == Find(conjunto[j]))){ //Comprobar que el nodo este libre
+                    Union(conjunto[i],conjunto[j]);
+                }
             }
         }
     }
